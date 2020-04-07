b3970efbf24c83b7a8b50e4e3a1077b81b2b6e1c
Subham Das <das.jishu25@gmail.com>
2020-04-07 20:49:55 +0530

    Add files via upload

--- forgot-password-page.php
+++ forgot-password-page.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Title of the page -->
-        <title>Company Website - Forgot Password</title>
+        <title>Flixathon - Forgot Password</title>
         
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         
--- index.php
+++ index.php
@@ -16,7 +16,7 @@ include('remember.php');
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Title of the page -->
-        <title>Company Website - Home</title>
+        <title>Flixathon - Home</title>
         
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         
