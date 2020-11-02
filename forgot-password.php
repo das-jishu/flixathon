cd9de24da71bd3a38f3eb095d312029f4b8360b4
Subham Das <das.jishu25@gmail.com>
2020-11-02 16:56:39 +0530

    Update forgot-password.php

--- forgot-password.php
+++ forgot-password.php
@@ -90,7 +90,7 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'flixathonmail@gmail.com';$mail->Password = 'flixathon_25';
+    $mail->Username = '**********@gmail.com';$mail->Password = '***********';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
     
